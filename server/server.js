3211e021dd5133697bc86be045f2a777a0d53101
Hung Vo <vqhung@gmail.com>
2016-12-10 16:31:24 +0700

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./db/models/todo');
-var {User} = require('./db/models/user');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 
@@ -21,6 +21,17 @@ app.post('/todos', (request, response) => {
   });
 });
 
+app.get('/todos', (request, response) => {
+  Todo.find().then((todos) => {
+    response.send({todos});
+  }, (e) => {
+    response.status(400).send(e);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000')
 });
+
+
+module.exports = {app};
